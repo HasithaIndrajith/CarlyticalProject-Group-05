a88d4d4d022c800e0c21c5f5b0883d4b1b7a2c68
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2022-11-17 20:22:57 +0530

    add customer set update

--- carlytic/src/pages/PredictCustomerSetPage.js
+++ carlytic/src/pages/PredictCustomerSetPage.js
@@ -11,8 +11,20 @@ function ProgressBarFunction(props) {
 function PredictCustomerSetPage() {
   const [file, setFile] = useState(null);
   const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [isError, setIsError] = useState("");
+  const [isClickable, setClickable] = useState(false);
   const onInputChange = (e) => {
-    setFile(e.target.files[0]);
+    setUploadPercentage(0);
+    const obj = e.target.files[0];
+    if (obj.type === "text/csv") {
+      setFile(e.target.files[0]);
+      setIsError(false);
+      setClickable(true);
+    } else {
+      setFile(e.target.files[0]);
+      setIsError(true);
+      setClickable(false);
+    }
     console.log(e.target.files[0]);
   };
   const submitForm = (e) => {
@@ -30,93 +42,130 @@ function PredictCustomerSetPage() {
               Math.round((progressEvent.loaded * 100) / progressEvent.total)
             )
           );
-          setTimeout(() => {
-            setUploadPercentage(0);
-          }, 10000);
         },
       })
       .then(() => {});
   };
+
   return (
-    <Fragment>
-      <link
-        rel="stylesheet"
-        href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
-        id="bootstrap-css"
-      />
-      <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-      <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <div
+      className="containPage"
+      style={{ backgroundImage: "url(/motor.jpeg)" }}
+    >
+      <Fragment>
+        <link
+          rel="stylesheet"
+          href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
+          id="bootstrap-css"
+        />
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
-      <div className="container">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <strong>Upload Files</strong> <small>Bootstrap files upload</small>
-          </div>
-          <div className="panel-body">
-            <h4>Select files from your computer</h4>
-            <form
-              action=""
-              method="post"
-              encType="multipart/form-data"
-              id="js-upload-form"
-              onSubmit={submitForm}
-              onDragOver={(e) => {
-                e.preventDefault();
-              }}
-              onDrop={(e) => {
-                e.preventDefault();
-                console.log(e);
-              }}
-            >
-              <div className="form-inline">
-                <div className="form-group">
-                  <input
-                    type="file"
-                    multiple
-                    name="files[]"
-                    id="js-upload-files"
-                    onChange={onInputChange}
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-sm btn-primary"
-                  id="js-upload-submit"
-                >
-                  Upload files
-                </button>
-              </div>
-            </form>
-
-            <h4>Or drag and drop files below</h4>
-            <div className="upload-drop-zone" id="drop-zone">
-              Just drag and drop files here
+        <div className="container">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <strong>Upload Files</strong>{" "}
             </div>
+            <div className="panel-body">
+              <h4>SELECT FILE FROM YOUR DEVICE</h4>
+              <div>
+                <form
+                  action=""
+                  method="post"
+                  encType="multipart/form-data"
+                  id="js-upload-form"
+                  onSubmit={submitForm}
+                >
+                  <div className="form-inline">
+                    <div className="form-group">
+                      <input
+                        type="file"
+                        multiple
+                        name="files[]"
+                        id="js-upload-files"
+                        onChange={onInputChange}
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="btn btn-sm btn-primary"
+                      id="js-upload-submit"
+                      // style={{ marginLeft: "50px" }}
+                      disabled={!isClickable}
+                    >
+                      Upload files
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <hr />
+              <h4>Or drag and drop files below</h4>
+              <div
+                className="upload-drop-zone"
+                id="drop-zone"
+                onDragStart={(e) => {
+                  e.preventDefault();
+                }}
+                onDragEnter={(e) => {
+                  e.preventDefault();
+                }}
+                onDragOver={(e) => {
+                  e.preventDefault();
+                }}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  setUploadPercentage(0);
+                  const obj = e.dataTransfer.files[0];
+                  if (obj.type === "text/csv") {
+                    setFile(e.dataTransfer.files[0]);
+                    setIsError(false);
+                    setClickable(true);
+                  } else {
+                    setFile(e.dataTransfer.files[0]);
+                    setIsError(true);
+                    setClickable(false);
+                  }
+                  console.log(e.dataTransfer.files[0].name);
+                }}
+                draggable
+              >
+                Just drag and drop files here
+              </div>
 
-            <ProgressBarFunction percentage={uploadPercentage} />
+              <ProgressBarFunction percentage={uploadPercentage} />
 
-            <div className="js-upload-finished">
-              <h3>Processed files</h3>
-              <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-success">
-                  <span className="badge alert-success pull-right">
-                    Success
-                  </span>
-                  image-01.jpg
-                </a>
-                <a href="#" className="list-group-item list-group-item-success">
-                  <span className="badge alert-success pull-right">
-                    Success
-                  </span>
-                  image-02.jpg
-                </a>
+              <div className="js-upload-finished">
+                <h3>SELECTED FILES</h3>
+                <div className="list-group">
+                  {isError === true ? (
+                    <a
+                      href="#"
+                      className="list-group-item list-group-item-warning"
+                    >
+                      <span className="badge alert-warning pull-right">
+                        File type not supported
+                      </span>
+                      {file !== null ? file.name : null}
+                    </a>
+                  ) : isError === false ? (
+                    <a
+                      href="#"
+                      className="list-group-item list-group-item-success"
+                    >
+                      <span className="badge alert-success pull-right">
+                        Success
+                      </span>
+                      {file !== null ? file.name : null}
+                    </a>
+                  ) : null}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <script src="./uploadPage.js"></script>
-    </Fragment>
+        <script src="./uploadPage.js"></script>
+      </Fragment>
+    </div>
   );
 }
 
